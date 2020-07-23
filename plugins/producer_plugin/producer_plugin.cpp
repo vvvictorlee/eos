123c512aaeae6d685dbf1c0a6fd7747f0b8e43b6
Brian Johnson <johnsonb@objectcomputing.com>
2020-07-23 16:40:10 -0500

    Fixed log

--- plugins/producer_plugin/producer_plugin.cpp
+++ plugins/producer_plugin/producer_plugin.cpp
@@ -1422,12 +1422,12 @@ producer_plugin_impl::start_block_result producer_plugin_impl::start_block() {
       const auto incoming_size = _unapplied_transactions.incoming_size();
       if( now < start_block_time ) {
          fc_dlog(_log, "Not producing block waiting for production window ${n} ${bt}", ("n", hbs->block_num + 1)("bt", block_time) );
-         fc_dlog(_log, "REMOVE Not producing waiting, size=${size}, incoming_size=${is}", ("size", size)("incoming_size", incoming_size));
+         fc_dlog(_log, "REMOVE Not producing waiting, size=${size}, incoming_size=${is}", ("size", size)("is", incoming_size));
          // start_block_time instead of block_time because schedule_delayed_production_loop calculates next block time from given time
          schedule_delayed_production_loop(weak_from_this(), calculate_producer_wake_up_time(start_block_time));
          return start_block_result::waiting_for_production;
       } else {
-         fc_dlog(_log, "REMOVE Possibly producing (not waiting), size=${size}, incoming_size=${is}", ("size", size)("incoming_size", incoming_size));
+         fc_dlog(_log, "REMOVE Possibly producing (not waiting), size=${size}, incoming_size=${is}", ("size", size)("is", incoming_size));
       }
    } else if (previous_pending_mode == pending_block_mode::producing) {
       // just produced our last block of our round
