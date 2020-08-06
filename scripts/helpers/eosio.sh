7357592f152a8b6bf9d37f61f120668153941fbf
Santhosh Kumaraswamy <kumaraswamy.santhosh@gmail.com>
2020-08-06 17:10:03 -0400

    Update eosio.sh
    Adding support for Ubuntu 20.04 into Release/2.0.x

--- scripts/helpers/eosio.sh
+++ scripts/helpers/eosio.sh
@@ -151,7 +151,7 @@ function prompt-mongo-install() {
 
 function ensure-compiler() {
     # Support build-essentials on ubuntu
-    if [[ $NAME == "CentOS Linux" ]] || [[ $VERSION_ID == "16.04" ]] || ( $PIN_COMPILER && [[ $VERSION_ID == "18.04" ]] ); then
+    if [[ $NAME == "CentOS Linux" ]] || [[ $VERSION_ID == "16.04" ]] || ( $PIN_COMPILER && ( [[ $VERSION_ID == "18.04" ]] || [[ $VERSION_ID == "20.04" ]] ) ); then
         export CXX=${CXX:-'g++'}
         export CC=${CC:-'gcc'}
     fi
